fcfdb5119c75abc0786d107a3f4ee1b9fdf9646a
SoEunKim03 <121374089+SoEunKim03@users.noreply.github.com>
2023-04-29 18:01:28 +0900

    Add files via upload

---
+++ Stick.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Stick {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int length = 64;
+        int cnt = 0;
+
+        while (input > 0) {
+            if (input > length) {
+
+
+            }
+            System.out.print(cnt);
+        }
+    }
+}
